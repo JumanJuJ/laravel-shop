b24fdcb1a9c59d6da0da6348be6d75b0a00fc4ed
Giovanni Chiussi <gec.chiussi@gmail.com>
2026-05-23 12:42:41 +0200

    initial commit

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use App\Models\order;
 use Illuminate\Database\Eloquent\Factories\Factory;
-#provaaaaa
+#provaaaaaaaaaaaaaaaaaaa
 /**
  * @extends Factory<order>
  */
