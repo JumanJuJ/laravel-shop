9d50c886d026835eb9652d6ebafdba11145c10d8
Giovanni Chiussi <gec.chiussi@gmail.com>
2026-05-23 12:44:06 +0200

    Update OrderFactory.php

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use App\Models\order;
 use Illuminate\Database\Eloquent\Factories\Factory;
-#provaaaaaaaaaaaaaaaaaaa
+#provaaaaaaaaaaaaaaaaaaaaaaa
 /**
  * @extends Factory<order>
  */
