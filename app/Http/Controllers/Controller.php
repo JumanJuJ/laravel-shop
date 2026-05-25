061a9f5e54f627fdb8c0aab3f5c157ac6ba3d831
Giovanni <gec.chiussi@gmail.com>
2026-05-25 09:30:29 +0200

    Post merge commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,5 @@
 <?php
 
 namespace App\Http\Controllers;
-
-<<<<<<< HEAD
-abstract class Controller
-{
-    //
-}
-=======
 abstract class Controller {}
->>>>>>> 8cae1d275c60c813ba168594876918bbbc7aa3ea
+?>
